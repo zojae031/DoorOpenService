b92bf5de7e8ff5ab91e8ec63e003d83888cab723
songmag <songmag@naver.com>
2018-12-05 18:53:20 +0900

    songmag finish

--- ArduinoDoor/ArduinoDoor.ino
+++ ArduinoDoor/ArduinoDoor.ino
@@ -6,7 +6,6 @@
 
 #define OPEN 2
 #define CLOSE 3
-#define WAIT 4
 
 #define STEP1 10
 #define STEP2 12
@@ -21,73 +20,73 @@ const int steps = 64;// 모터별 스탭 수
 SoftwareSerial BlueTooth(BT_RX, BT_TX);
 Stepper stepper(steps,STEP1,STEP2,STEP3,STEP4); 
 void setup() {
-  BlueTooth.begin(9600);
-  pinMode(OPEN, OUTPUT);
-  pinMode(CLOSE, OUTPUT);
-  pinMode(WAIT,OUTPUT);
-  pinMode(SENSOR,INPUT);
-  digitalWrite(CLOSE,HIGH);
-  stepper.setSpeed(220);//최고 속도
-  pinMode(STEP1,OUTPUT);
-  pinMode(STEP2,OUTPUT);
-  pinMode(STEP3,OUTPUT); 
-  pinMode(STEP4,OUTPUT);
-  //none block을 위한 pinmode 변경
+	BlueTooth.begin(9600);
+	pinMode(OPEN, OUTPUT);
+	pinMode(CLOSE, OUTPUT);
+	pinMode(SENSOR,INPUT);
+	digitalWrite(CLOSE,HIGH);
+	stepper.setSpeed(220);//최고 속도
+	pinMode(STEP1,OUTPUT);
+	pinMode(STEP2,OUTPUT);
+	pinMode(STEP3,OUTPUT); 
+	pinMode(STEP4,OUTPUT);
+	//none block을 위한 pinmode 변경
 }
 
 
 void loop() {
-  if(BlueTooth.available()) {
-    char data = BlueTooth.read(); // 블루투스로부터 데이터를 받아옴
-    int walk;
-    if (data == '1') { //만약 입력된 데이터가 1이면
-      walk = d_open();
-      switch(walk)//차후 지나간것과 지나가지 않았을때 보내는 데이터가 다를 것을 고려 한 Switch case 문
-      {
-	case 0:
-	       BlueTooth.write(100);//기기에 1을 보냄
-	case 1:
-	       BlueTooth.write(100);//지나간 경우
-x      }
-    }
-  }
+	if(BlueTooth.available()) {
+		char data = BlueTooth.read(); // 블루투스로부터 데이터를 받아옴
+		int walk;
+		if (data == '1') 
+		{ //만약 입력된 데이터가 1이면
+			walk = d_open();
+			switch(walk)//차후 지나간것과 지나가지 않았을때 보내는 데이터가 다를 것을 고려 한 Switch case 문
+			{
+				case 0:
+					BlueTooth.write(100);//기기에 1을 보냄
+				case 1:
+					BlueTooth.write(100);//지나간 경우
+			}
+		}
+	}
 }
 int d_open()
 {
-  int count=0;
-  unsigned long currenttime = millis();
-  unsigned long finishtime = currenttime+10000;
-  unsigned long savetime = currenttime+6000;
-  
-  stepper.step(90/steps);
-  digitalWrite(CLOSE,LOW);
-  digitalWrite(OPEN,HIGH);
-  while(digitalRead(SENSOR) != LOW)
-  {
-     if(finishtime - currenttime <= 5000)
-     {
-      if((digitalRead(OPEN)==HIGH) && (currenttime >= savetime))
-      {
-        digitalWrite(OPEN,LOW);
-	      digitalWrite(WAIT,HIGH);
-        savetime = currenttime+1000;
-      }
-      else if((digitalRead(OPEN)==LOW) && (currenttime >= savetime))
-      {
-	digitalWrite(WAIT,LOW);        
-	savetime = currenttime+1000;
-      }
-    }
-    if(finishtime < currenttime)
-    {
-      digitalWrite(OPEN,LOW);
-      digitalWrite(CLOSE,HIGH);
-      return 0;
-    }
-    currenttime = millis();  
-  }
-  digitalWrite(OPEN,LOW);
-  digitalWrite(CLOSE,HIGH);
-  stepper.step(-(90/steps));
-  return 1;
+	int count=0;
+	unsigned long currenttime = millis();
+	unsigned long finishtime = currenttime+10000;
+	unsigned long savetime = currenttime+6000;
+//nonblock system 을 위해서 만든 millis 를 통한 시간 측정
+	stepper.step(-512);//1024 = 180도 512 90도 
+	digitalWrite(CLOSE,LOW);
+	digitalWrite(OPEN,HIGH);
+	while(digitalRead(SENSOR) != LOW)
+	{
+		if(finishtime - currenttime <= 5000)//5초를 지나게 되면 불이 깜빡이게끔 만든 부분
+		{
+			if((digitalRead(OPEN)==HIGH) && (currenttime >= savetime))
+			{
+				digitalWrite(OPEN,LOW);
+				savetime = currenttime+1000;
+			}
+			else if((digitalRead(OPEN)==LOW) && (currenttime >= savetime))
+			{
+				digitalWrite(OPEN,HIGH);
+				savetime = currenttime+1000;
+			}
+		}
+		if(finishtime < currenttime)
+		{
+			digitalWrite(OPEN,LOW);
+			digitalWrite(CLOSE,HIGH);
+			stepper.step(512);
+			return 0;
+		}
+		currenttime = millis();  
+	}
+	digitalWrite(OPEN,LOW);
+	digitalWrite(CLOSE,HIGH);
+	stepper.step(512);
+	return 1;
 }
